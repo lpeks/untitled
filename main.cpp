be9f41047c1981aa1afa87571d91cb8e747a34cd
Lucie Peksová <peksoluc@fel.cvut.cz>
2018-10-02 19:26:31 +0200

    another one

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Oh, something new here, guys\n";
+    std::cout << "Now, i would like to merche these thing. Magic will come.\n";
     return 0;
 }
